859e626e2c20e5a24c92baf0a125ee51695cb571
Pablo V <pablo.valdesogo@gmail.com>
2023-06-15 20:39:38 -0300

    Creacion

--- src/main/java/com/vco/f1extreme/service/PlayerSelection.java
+++ src/main/java/com/vco/f1extreme/service/PlayerSelection.java
@@ -1,4 +1,13 @@
 package main.java.com.vco.f1extreme.service;
+import main.java.com.vco.f1extreme.model.Player;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 public class PlayerSelection {
+    private List<Player> players;
+
+    public PlayerSelection(List<Player> players) {
+        this.players = players;
+    }
 }
