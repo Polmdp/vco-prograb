25e6415c49ef1a626b10a550a4238ac97b7fe20c
joaquin <ojoaquin057@gmail.com>
2023-06-15 19:51:39 -0300

    .

--- src/main/java/com/vco/f1extreme/model/Tire.java
+++ src/main/java/com/vco/f1extreme/model/Tire.java
@@ -2,6 +2,11 @@ package main.java.com.vco.f1extreme.model;
 
 public class Tire {
     private String soft{
+        //Proveen mayor agarre, mayores velocidades, pero menor
+        //durabilidad y mayor consumo. Son los neumáticos más rápidos
+        //pero también los que se desgastan más rápidamente, lo que
+        //probablemente obligue a realizar paradas técnicas para
+        //reemplazarlos.
         public String getSoft() {
             return soft;
         }
@@ -11,6 +16,9 @@ public class Tire {
         }
     }
     private String hard{
+        //Ofrecen la mayor durabilidad, pero con menor agarre y, por ende,
+        //es más difícil la maniobrabilidad a altas velocidades (es decir, el
+        //auto no podrá alcanzar la máxima velocidad)
         public String getHard() {
             return hard;
         }
@@ -20,6 +28,7 @@ public class Tire {
         }
     }
     private String medium{
+        //Son una combinación entre los neumáticos Soft y Hard
         public String getMedium() {
             return medium;
         }
@@ -29,6 +38,9 @@ public class Tire {
         }
     }
     private String wet{
+        //Son los que mejor performance ofrecen en condiciones de lluvia.
+        //Ofrecen gran durabilidad pero sólo si la pista está mojada, en pista
+        //seca se rompen rápidamente.
         public String getWet() {
             return wet;
         }
