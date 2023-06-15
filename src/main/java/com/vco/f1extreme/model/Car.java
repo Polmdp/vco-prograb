bff004d6ade1f75e976ba00770928f119590bee8
Pablo V <pablo.valdesogo@gmail.com>
2023-06-15 20:01:38 -0300

    Creacion

--- src/main/java/com/vco/f1extreme/model/Car.java
+++ src/main/java/com/vco/f1extreme/model/Car.java
@@ -1,11 +1,14 @@
 package main.java.com.vco.f1extreme.model;
 
 public class Car {
+    private String color;
 
-    private String color;{
-        public String getColor() {
-            return color;
-        }
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
     private int number;{
         public int getNumber() {
