79c3af0c0b8e13801fe64580232a9d966d0375eb
joaquin <ojoaquin057@gmail.com>
2023-06-15 19:48:27 -0300

    metodos y atributos de car y tire

--- src/main/java/com/vco/f1extreme/model/Car.java
+++ src/main/java/com/vco/f1extreme/model/Car.java
@@ -1,5 +1,61 @@
 package main.java.com.vco.f1extreme.model;
 
 public class Car {
-    private String color;
+
+    private String color;{
+        public String getColor() {
+            return color;
+        }
+    }
+    private int number;{
+        public int getNumber() {
+            return number;
+        }
+    }
+    private String brand;{
+    public String getBrand() {
+            return brand;
+        }
+    }
+    private float weight;{
+    public float getWeight() {
+            return weight;
+        }
+}
+    private float maxspeed{
+        public float getMaxspeed() {
+            return maxspeed;
+        }
+    }
+    private float acceleration{
+        public float getAcceleration() {
+            return acceleration;
+        }
+    }
+    private float fuelconsumption{
+        public float getFuelconsumption() {
+            return fuelconsumption;
+        }
+    }
+
+    //metodos............
+    public void stop(){
+
+    }
+    public void start(){
+
+    }
+    //valores entre 1 y 100, a mayor valor, mejor desempeño
+    public int perfcurves(){
+        //Determina cómo se desempeña el auto en aceleraciones rápidas
+        //y/o rectas prolongadas para sobrepasar a otros autos
+    }
+    public int perfoverruns(){
+        //Determina cómo es el comportamiento del auto en las curvas
+    }
+    public int reliability(){
+        //Determina qué tan confiable es el auto: A menor valor de este
+        //atributo, mayores probabilidades de que vaya a abandonar
+        //durante la carrera por desperfectos mecánicos
+    }
 }
--- src/main/java/com/vco/f1extreme/model/Tire.java
+++ src/main/java/com/vco/f1extreme/model/Tire.java
@@ -1,4 +1,40 @@
 package main.java.com.vco.f1extreme.model;
 
 public class Tire {
+    private String soft{
+        public String getSoft() {
+            return soft;
+        }
+
+        public void setSoft(String soft) {
+            this.soft = soft;
+        }
+    }
+    private String hard{
+        public String getHard() {
+            return hard;
+        }
+
+        public void setHard(String hard) {
+            this.hard = hard;
+        }
+    }
+    private String medium{
+        public String getMedium() {
+            return medium;
+        }
+
+        public void setMedium(String medium) {
+            this.medium = medium;
+        }
+    }
+    private String wet{
+        public String getWet() {
+            return wet;
+        }
+
+        public void setWet(String wet) {
+            this.wet = wet;
+        }
+    }
 }
