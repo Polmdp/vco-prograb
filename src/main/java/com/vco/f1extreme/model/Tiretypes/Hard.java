96e6284d8aa899118b718fbe04383ce78cc5f487
augusto <augustocastro@gmail.com>
2023-06-16 11:06:00 -0300

    .

--- src/main/java/com/vco/f1extreme/model/Tiretypes/Hard.java
+++ src/main/java/com/vco/f1extreme/model/Tiretypes/Hard.java
@@ -2,7 +2,7 @@ package main.java.com.vco.f1extreme.model.Tiretypes;
 
 import main.java.com.vco.f1extreme.model.Tire;
 
-public class hard extends Tire {
+public class Hard extends Tire {
 //Ofrecen la mayor durabilidad, pero con menor agarre y, por ende,
 //es más difícil la maniobrabilidad a altas velocidades (es decir, el
 //auto no podrá alcanzar la máxima velocidad)
