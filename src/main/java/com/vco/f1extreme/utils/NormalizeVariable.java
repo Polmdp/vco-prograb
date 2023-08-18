139ba3ac9be6a4ded785700bf8930b39200ec743
Pablo V <pablo.valdesogo@gmail.com>
2023-08-18 11:55:45 -0300

    Creacion

--- src/main/java/com/vco/f1extreme/utils/NormalizeVariable.java
+++ src/main/java/com/vco/f1extreme/utils/NormalizeVariable.java
@@ -1,6 +1,8 @@
 package main.java.com.vco.f1extreme.utils;
 
 public class NormalizeVariable {
+
+
     public static double normalizeVariable(double variable, double minValue, double maxValue) {
         double normalizedVariable = (variable - minValue) / (maxValue - minValue) * 5;
         double roundedNormalizedVariable = Math.round(normalizedVariable * 100) / 100.0;
